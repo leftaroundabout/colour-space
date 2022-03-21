42d6605102e2e7a6336eb763c486565db225453f
Justus Sagemüller <jsag@hvl.no>
2022-03-21 19:35:38 +0100

    First nontrivial test.
    Which fails.

--- test/tasty/test.hs
+++ test/tasty/test.hs
@@ -25,8 +25,11 @@
 
 import Math.Manifold.Core.PseudoAffine
 import Data.Manifold.Types
+import Data.Manifold.WithBoundary
+import Data.VectorSpace
 
 import Data.Colour.Manifold
+import Data.Colour.Manifold.Internal
 import Data.Colour
 import Data.Colour.SRGB.Linear
 
@@ -41,12 +44,17 @@ main = do
   defaultMain $ testGroup "Tests"
    [ testGroup "Sanity checks"
     [ testProperty "Self equality"
-     $ \(c :: Colour ℝ) -> c ≈≈ c
+     $ \(c :: Colour ℝ) -> c ≈≈≈ c
     , testProperty "Equality nontriviality"
-     . QC.expectFailure $ \(c :: Colour ℝ) d -> c ≈≈ d
+     . QC.expectFailure $ \(c :: Colour ℝ) d -> c ≈≈≈ d
     ]
    , testGroup "Manifold laws"
-    [  ]
+    [ testProperty "Zero displacement"
+     $ \(c :: Colour ℝ) -> case toInterior c of
+          Nothing -> undefined
+          Just ci -> case c .+^| zeroV of
+            Right c' -> c'≈≈≈ci
+    ]
    ]
 
 
@@ -57,18 +65,29 @@ class NearEq a where
 
 instance NearEq ℝ where
   n ≈≈ m
-   | n>=0, m>=0, n<=1, m<=1  = abs (n-m) < 1e-9
+   | abs n<=1, m<=1  = abs (n-m) < 1e-9
+   | otherwise       = abs (n-m) < 1e-9 * (abs n + abs n)
 instance (NearEq a, NearEq b) => NearEq (a,b) where
   (x,y) ≈≈ (ξ,υ) = x≈≈ξ && y≈≈υ
 instance (NearEq a, NearEq b, NearEq c) => NearEq (a,b,c) where
   (x,y,z) ≈≈ (ξ,υ,ζ) = x≈≈ξ && y≈≈υ && z≈≈ζ
 instance NearEq a => NearEq (RGB a) where
   RGB r g b ≈≈ RGB ρ γ β = r≈≈ρ && g≈≈γ && b≈≈β
+instance NearEq a => NearEq (Maybe a) where
+  Just p ≈≈ Just q = p≈≈q
+  Nothing ≈≈ Nothing = True
+  _ ≈≈ _ = False
+instance (NearEq a, NearEq b) => NearEq (Either a b) where
+  Left p ≈≈ Left q = p≈≈q
+  Right p ≈≈ Right q = p≈≈q
+  _ ≈≈ _ = False
 instance NearEq D¹ where
   D¹ p ≈≈ D¹ q = abs (p-q) < 1e-9
 
 instance (NearEq a, Fractional a) => NearEq (Colour a) where
   c ≈≈ ζ = toRGB c ≈≈ toRGB ζ
+instance NearEq ColourNeedle where
+  ColourNeedle v ≈≈ ColourNeedle w = v ≈≈ w
 instance (Fractional a, Random a) => QC.Arbitrary (Colour a) where
   arbitrary = rgb <$> QC.choose (0,1) <*> QC.choose (0,1) <*> QC.choose (0,1)
 
